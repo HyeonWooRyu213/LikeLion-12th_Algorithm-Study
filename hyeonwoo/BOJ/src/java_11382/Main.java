6fdcdf66413405536f538f564996544d1a2d2a2c
HyeonWooRyu213 <fbgusdn05@gmail.com>
2024-09-14 23:20:57 +0900

    feature: 깃 과제

---
+++ hyeonwoo/BOJ/src/java_11382/Main.java
@@ -0,0 +1,15 @@
+package java_11382;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+
+        Long a = sn.nextLong();
+        Long b = sn.nextLong();
+        Long c = sn.nextLong();
+
+        System.out.println(a + b + c);
+    }
+}
