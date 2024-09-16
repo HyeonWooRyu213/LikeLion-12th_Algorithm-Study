5bea0ad57293911a8171b645952fbafec4d07988
HyeonWooRyu213 <fbgusdn05@gmail.com>
2024-09-16 15:15:20 +0900

    이게 맞을까

--- hyeonwoo/BOJ/src/git/Ggit.java
+++
@@ -1,7 +0,0 @@
-package git;
-
-public class Ggit {
-    public static void main(String[] args) {
-        System.out.println("난 깃이 싫다.");
-    }
-}
--- hyeonwoo/BOJ/src/git/Main.java
+++ hyeonwoo/BOJ/src/git/Main.java
@@ -2,6 +2,6 @@ package git;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("깃 과제");
+        System.out.println("이게 맞나");
     }
 }
